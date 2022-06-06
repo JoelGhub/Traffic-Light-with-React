41a2d2d20e329ccf3681572895f5fad89183e692
JoelGhub <font.joel@gmail.com>
2022-06-06 14:48:20 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,115 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { func } from "prop-types";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("red");
+	const [color2, setColor2] = useState("yellow");
+	const [color3, setColor3] = useState("green");
+	const [color4, setColor4] = useState(" ");
+
+	function setLightRed() {
+		if (color === "red" && color2 === "yellow" && color3 === "green") {
+			setColor("red glow");
+			setColor2("yellow");
+			setColor3("green");
+		} else if (
+			color === "red" &&
+			color2 === "yellow glow" &&
+			color3 === "green"
+		) {
+			setColor("red glow");
+			setColor2("yellow");
+			setColor3("green");
+		} else if (
+			color === "red" &&
+			color2 === "yellow" &&
+			color3 === "green glow"
+		) {
+			setColor("red glow");
+			setColor2("yellow");
+			setColor3("green");
+		} else {
+			setColor("red");
+		}
+	}
+	function setLightYellow() {
+		if (color === "red glow" && color2 === "yellow" && color3 === "green") {
+			setColor("red");
+			setColor2("yellow glow");
+			setColor3("green");
+		} else if (
+			color === "red" &&
+			color2 === "yellow" &&
+			color3 === "green"
+		) {
+			setColor("red");
+			setColor2("yellow glow");
+			setColor3("green");
+		} else if (
+			color === "red" &&
+			color2 === "yellow" &&
+			color3 === "green glow"
+		) {
+			setColor("red");
+			setColor2("yellow glow");
+			setColor3("green");
+		} else {
+			setColor2("yellow");
+		}
+	}
+	function setLightGreen() {
+		if (
+			color === "red glow" ||
+			(color2 === "yellow glow" && color3 === "green")
+		) {
+			setColor("red");
+			setColor2("yellow");
+			setColor3("green glow");
+		} else if (
+			color === "red" &&
+			color2 === "yellow" &&
+			color3 === "green"
+		) {
+			setColor("red");
+			setColor2("yellow");
+			setColor3("green glow");
+		} else {
+			setColor3("green");
+		}
+	}
+	function setLightPurple() {
+		if (color4 === " ") {
+			setColor4("light purple ");
+		} else {
+		}
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="btn-div">
+				<button onClick={setLightPurple}>New Purple light</button>
+			</div>
+
+			<img className="img-src" src=""></img>
+			<div className=" positioning ">
+				<div className="stick"></div>
+				<ol className=" parent-div ">
+					<li
+						className={" light " + color}
+						onClick={setLightRed}></li>
+					<li
+						className={" light " + color2}
+						onClick={setLightYellow}></li>
+					<li
+						className={" light " + color3}
+						onClick={setLightGreen}></li>
+					<li
+						style={{ listStyle: "none" }}
+						className={" " + color4}></li>
+				</ol>
+			</div>
+		</>
 	);
 };
 
